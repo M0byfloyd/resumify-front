8a03073b3504da84685d488bff84bab8f81c5adf
M0byfloyd <54885010+M0byfloyd@users.noreply.github.com>
2021-09-26 03:28:44 +0200

    Project install (#1)

---
+++ pages/Baptiste.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="baptise-title__container">
+      <h1 class="baptise-title__title">
+        Salut enculé
+      </h1>
+    </div>
+    <div class="baptise-title__container--bg-hotpink">
+      <h2 class="baptise-title__title">
+        Baptiste pd
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Baptiste"
+}
+</script>
+
+<style scoped>
+
+</style>
