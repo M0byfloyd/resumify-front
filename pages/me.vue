fe4a5b6bbbfa615679ade89b75c302ad84a335e7
M0byfloyd <alexandre.robert2810@gmail.com>
2021-11-28 21:46:08 +0100

    Ajout d'info sur la page /me

--- pages/me.vue
+++ pages/me.vue
@@ -1,18 +1,22 @@
 <template>
   <v-container v-if="token">
     <v-row>
-      <v-col>
+      <v-col cols="3">
         <v-avatar
           color="primary"
           rounded
-          size="300"
+          size="200"
         >
-          <img :src="userImage" :alt="`${userId} avatar`" />
+          <img :src="userImage" :alt="`${userId} avatar`"/>
         </v-avatar>
       </v-col>
-      <v-col>
-        <h1>Bienvenue <span>{{ displayName }}</span></h1>
-        <a href="">Voir votre profil Spotify</a>
+      <v-col cols="9">
+        <h1>
+          <span v-if="isPremium" title="Vous êtes premium !">⭐</span>
+          Bienvenue <span>{{ displayName }}</span>
+        </h1>
+        <p>{{ followers }} follower(s)</p>
+        <a target="_blank" :href="accountLink">Voir votre profil Spotify</a>
       </v-col>
     </v-row>
   </v-container>
@@ -32,6 +36,18 @@ export default {
       displayName: 'user/getDisplayName',
       token: 'user/getToken',
     }),
+
+    accountLink() {
+      return `https://open.spotify.com/user/${this.infos?.id}`;
+    },
+
+    isPremium() {
+      return this.infos?.product;
+    },
+
+    followers() {
+      return this.infos.followers.total;
+    }
   },
 
   created() {
